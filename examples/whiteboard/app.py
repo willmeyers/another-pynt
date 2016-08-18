3141c2c6e903837f93be0801f651f083aabc54bb
Will Meyers <williamgmeyers@gmail.com>
2016-08-17 23:30:01 -0400

    can send message to server on event

--- examples/whiteboard/app.py
+++ examples/whiteboard/app.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import socket
 
 
 class App:
@@ -11,14 +12,15 @@ class App:
         self.client_color = (random.randint(50, 255),random.randint(50, 255),random.randint(50, 255))
         self.is_drawing = False
 
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setblocking(False)
+
         self.running = True
 
     def update(self):
         self.mouse_pos = pygame.mouse.get_pos()
 
     def render(self, window):
-        #self.window.fill((255, 255, 255))
-
         pygame.display.flip()
 
     def handle_events(self, events):
@@ -26,13 +28,16 @@ class App:
             if e.type == pygame.QUIT:
                 self.running = False
             if e.type == pygame.MOUSEBUTTONDOWN:
-                pygame.draw.circle(self.window, self.client_color, self.mouse_pos, 10)
+                pygame.draw.circle(self.window, self.client_color, self.mouse_pos, 5)
                 self.is_drawing = True
             if e.type == pygame.MOUSEBUTTONUP:
                 self.is_drawing = False
             if e.type == pygame.MOUSEMOTION:
                 if self.is_drawing:
-                    pygame.draw.circle(self.window, self.client_color, self.mouse_pos, 10)
+                    pygame.draw.circle(self.window, self.client_color, self.mouse_pos, 5)
+                    self.sock.sendto(b'HEY', ('localhost', 8080))
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_c:
+                self.window.fill((0, 0, 0))
 
     def run(self):
         self.update()
