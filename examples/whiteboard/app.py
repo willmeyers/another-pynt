c071e90a1e61a1d5b5baccb84732faae80031941
Will Meyers <williamgmeyers@gmail.com>
2016-08-22 16:29:15 -0400

    added client color

--- examples/whiteboard/app.py
+++ examples/whiteboard/app.py
@@ -10,9 +10,10 @@ class App:
     def __init__(self):
         self.window = pygame.display.set_mode((640, 480))
         self.clock = pygame.time.Clock()
+        self.window.fill((255, 255, 255))
 
         self.mouse_pos = None
-        self.client_color = (random.randint(50, 255),random.randint(50, 255),random.randint(50, 255))
+        self.client_color = (0, 0, 0)
         self.is_drawing = False
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -34,7 +35,6 @@ class App:
             if e.type == pygame.QUIT:
                 self.running = False
             if e.type == pygame.MOUSEBUTTONDOWN:
-                pygame.draw.circle(self.window, self.client_color, self.mouse_pos, 5)
                 m = b'DRAW'+str(self.mouse_pos[0]).encode()+str(self.mouse_pos[1]).encode()
                 self.sock.sendto(m, ('localhost', 8080))
                 self.is_drawing = True
@@ -42,11 +42,10 @@ class App:
                 self.is_drawing = False
             if e.type == pygame.MOUSEMOTION:
                 if self.is_drawing:
-                    pygame.draw.circle(self.window, self.client_color, self.mouse_pos, 5)
                     m = b'DRAW'+str(self.mouse_pos[0]).encode()+str(self.mouse_pos[1]).encode()
                     self.sock.sendto(m, ('localhost', 8080))
             if e.type == pygame.KEYDOWN and e.key == pygame.K_c:
-                self.window.fill((0, 0, 0))
+                self.window.fill((255, 255, 255))
             if e.type == pygame.KEYDOWN and e.key == pygame.K_q:
                 self.sock.sendto(b'CONN', ('localhost', 8080))
             if e.type == pygame.KEYDOWN and e.key == pygame.K_s:
@@ -56,7 +55,7 @@ class App:
         self.update()
         self.render(self.window)
         self.handle_events(pygame.event.get())
-        self.clock.tick(60)
+        self.clock.tick()
 
         if self.server:
             self.server.run()
@@ -69,8 +68,7 @@ class App:
                 if message[:4] == b'DRAW':
                     x = int(message[4:7])
                     y = int(message[7:])
-                    pygame.draw.circle(self.window, (255, 255, 255), (x, y), 5)
-
+                    pygame.draw.circle(self.window, (255, 0, 0), (x, y), 5)
 
     def close(self):
         pygame.quit()
